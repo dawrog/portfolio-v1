16e6119c01f236af492b0aaf890f2158483a230e
dawrog <dawid.rogal@protonmail.com>
2017-12-30 23:48:25 +0100

    added mobile menu

--- menu.js
+++ menu.js
@@ -6,5 +6,12 @@ $(this).effect("bounce", { times:2 }, 900);
 
 });
     
+$(".burger-nav").on("click", function(){
+        
+    $("header nav ul").toggleClass("open");
+    
+    
+    });
+    
 });
 
